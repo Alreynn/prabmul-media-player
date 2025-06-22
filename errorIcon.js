ab95a64734eba563a563f970b4d7b9d307c0dd6f
Alreynn <aliung04@gmail.com>
2025-06-22 21:34:53 +0700

    Changed the error message algorithm.

--- errorIcon.js
+++ errorIcon.js
@@ -1,29 +1,43 @@
-function reload() {
-    window.location.reload();
+function declareError(errorTitleMsg, errorDescriptionMsg) {
+    errorMessage.innerText = errorTitleMsg;
+    errorMessageDescription.innerText = errorDescriptionMsg;
+}
+const errorTitle = {
+    iconNotLoaded: 'Kayaknya kamu error nih.',
+    mediaTooSmall: 'Layar terlalu kecil!',
+}
+const errorDescription = {
+    iconNotLoaded: 'Coba refresh?',
+    mediaTooSmall: 'Gunakan perangkat lain.',
 }
 
+const errorMessage = document.querySelector('.errorScreen .errorContainer h2');
+const errorMessageDescription = document.querySelector('.errorScreen .errorContainer p');
 window.onload = document.fonts.addEventListener('loadingdone', () => {
     document.getElementById('errorScreen').style.display = 'none';
-
+    
     const elementCheck = document.createElement('i');
-    elementCheck.className = 'fa-solid';
-    document.body.appendChild(elementCheck);
-    const fontFamily = window.getComputedStyle(elementCheck).getPropertyValue('font-family');
+        elementCheck.className = 'fa-solid';
+        document.body.appendChild(elementCheck);
+        const fontFamily = window.getComputedStyle(elementCheck).getPropertyValue('font-family');
     if (!fontFamily.includes('Font Awesome')) {
         document.getElementById('errorScreen').style.display = 'block';
+        declareError(errorTitle.iconNotLoaded, errorDescription.iconNotLoaded);
     }
     elementCheck.remove();
 })
+
 const mediaLimit = window.matchMedia("(max-width: 240px)");
 function media() {
-    const errorMessage = document.querySelector('.errorScreen .errorContainer h2');
-    const errorMessageDescription = document.querySelector('.errorScreen .errorContainer p');
     if (mediaLimit.matches) {
         document.getElementById('errorScreen').style.display = 'block';
-        errorMessage.innerText = 'Layar terlalu kecil!';
-        errorMessageDescription.innerText = 'Harap ubah resolusi layar dan refresh.';
+        declareError(errorTitle.mediaTooSmall, errorDescription.mediaTooSmall);
     }
 }
 setInterval(() => {
-    mediaLimit.addEventListener("change", media);
+    media();
 }, 100)
+
+function reload() {
+    window.location.reload();
+}
